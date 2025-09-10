5681accd410ddb3a87a1a115ff044bf41f4fd121
diana7899930 <diankadiana2308@gmail.com>
2025-09-10 10:51:05 +0300

    Create diana1.py

---
+++ diana1.py
@@ -0,0 +1,34 @@
+number = 58
+metrs = 25,7
+place = 'Miami'
+itscool = True
+books = ["Silver eye" , "Nghtmers of Shadows"]
+coordinates = (1230, 56, -3908)
+users = {"Vlad": "Taras"}
+none = None
+othernumbers = {1, 2, 3, 4}
+file = b"Hello World"
+
+
+print(file)
+print(othernumbers)
+print(none)
+print(users)
+print(coordinates)
+print(books)
+print(itscool)
+print(place)
+print(metrs)
+print(number)
+
+
+print(type(file))
+print(type(othernumbers))
+print(type(none))
+print(type(users))
+print(type(coordinates))
+print(type(books))
+print(type(itscool))
+print(type(place))
+print(type(metrs))
+print(type(number))
