bf44bfbd6106f321c2d38f950ef80f70e244fbd8
diana7899930 <diankadiana2308@gmail.com>
2025-11-26 08:56:58 +0200

    Create decorator.py

---
+++ laba6/decorator.py
@@ -0,0 +1,25 @@
+ def wrapper(*args, **kwargs):             
+
+        new_args = []                         
+
+        for value in args:
+            if isinstance(value, str):       
+                utf_text = value.encode("utf-8")   
+                new_args.append(utf_text)         
+            else:
+                new_args.append(value)             
+
+        new_kwargs = {}                      
+
+      
+        for key, value in kwargs.items():
+            if isinstance(value, str):
+                utf_text = value.encode("utf-8")
+                new_kwargs[key] = utf_text
+            else:
+                new_kwargs[key] = value
+
+        result = func(*new_args, **new_kwargs)
+        return result
+
+    return wrapper
